4013db9d965ad3c2c9a5f4daa36052b6d5854ff3
Jay Ribakove <jrib6@yahoo.com>
2016-05-16 21:49:05 -0400

    refactoring to separate component files and ES6

---
+++ scripts/components/StorePicker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Navigation } from 'react-router';
+import h from '../helpers'
+import reactMixin from 'react-mixin';
+
+class StorePicker extends React.Component {
+  // mixins : [Navigation],
+  goToStore(event) {
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
+    )
+  }
+}
+
+reactMixin.onClass(StorePicker, Navigation);
+
+
+export default StorePicker;
